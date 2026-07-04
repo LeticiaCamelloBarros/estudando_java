4a50c1d659d93f8a10db89985c0260146a0ac685
leticia camello barros <lsncb@cesar.school>
2026-07-03 23:12:48 -0300

    Update Funcionario.java
    concertando classe

--- Escola_heranca/Funcionario.java
+++ Escola_heranca/Funcionario.java
@@ -1,5 +1,21 @@
 package escola;
 
-public class Funcionario {
-
+public class Funcionario extends Pessoa{
+private char setor ; 
+private boolean trabalhando ;
+public char getSetor() {
+	return setor;
+}
+public void setSetor(char setor) {
+	this.setor = setor;
+}
+public boolean getTrabalhando() {
+	return this.trabalhando;
+}
+public void setTrabalhando(boolean trabalhando) {
+	this.trabalhando = trabalhando;
+} 
+public void mudarTrabalhando() {
+	this.setTrabalhando(!(this.trabalhando));
+}
 }
