943e3f4377e700a397e3a93ef16c07de6602e64d
leticia camello barros <lsncb@cesar.school>
2026-07-03 23:14:43 -0300

    Implement Professor class extending Pessoa
    concertando classe importada errado

--- Escola_heranca/Professor.java
+++ Escola_heranca/Professor.java
@@ -1,5 +1,26 @@
+
 package escola;
 
-public class Professor {
-
+public class Professor extends Pessoa {
+	//professor é filha de pessoa
+	//
+	
+  private char especialidade ; 
+  private double salario ; 
+  public void receberAumento(int aum) {
+	  this.setSalario(this.getSalario()+aum);
+  }
+  public char getEspecialidade() {
+	return especialidade;
+  }
+  public void setEspecialidade(char especialidade) {
+	this.especialidade = especialidade;
+  }
+  public double getSalario() {
+	return salario;
+  }
+  public void setSalario(double salario) {
+	this.salario = salario;
+  }
+  
 }
