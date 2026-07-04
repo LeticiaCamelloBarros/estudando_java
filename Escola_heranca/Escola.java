9b67b596daac321c1e1623eee1de0b7ede64265c
leticia camello barros <lsncb@cesar.school>
2026-07-03 23:12:25 -0300

    Update Escola.java
    concertando classe importada errado

--- Escola_heranca/Escola.java
+++ Escola_heranca/Escola.java
@@ -3,8 +3,16 @@ package escola;
 public class Escola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+    Pessoa p1 = new Pessoa();
+    Aluno p2 = new Aluno ();
+    Professor p3 = new Professor();
+    Funcionario p4 = new Funcionario();
+    p1.setIdade(17);
+    p1.setNome("pedro");
+    p2.setIdade(18);
+    p3.setNome("Roger");
+    p2.setCurso("biologia quantica");
+    
 	}
 
 }
