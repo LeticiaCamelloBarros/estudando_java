8f434c432a8a5efb52859a801ef435e1b8097b38
leticia camello barros <lsncb@cesar.school>
2026-07-03 23:14:09 -0300

    Update Pessoa.java
    concertando classe pessoa que foi importada errado

--- Escola_heranca/Pessoa.java
+++ Escola_heranca/Pessoa.java
@@ -1,5 +1,34 @@
 package escola;
 
 public class Pessoa {
-
+	//Pessoa é a superclasse das outras três 
+  private int idade ; 
+  @Override
+public String toString() {
+	return "Pessoa [idade=" + idade + ", sexo=" + sexo + ", nome=" + nome + "]";
+}
+  private char sexo ; 
+  private String nome ;
+ //getters e setters ;  
+  public int getIdade() {
+	return idade;
+  }
+  public void setIdade(int idade) {
+	this.idade = idade;
+  }
+  public char getSexo() {
+	return sexo;
+  }
+  public void setSexo(char sexo) {
+	this.sexo = sexo;
+  }
+  public String getNome() {
+	return nome;
+  }
+  public void setNome(String nome) {
+	this.nome = nome;
+  } 
+  public void fazerAniv() {
+	  this.setIdade(this.getIdade()+1);
+  }
 }
