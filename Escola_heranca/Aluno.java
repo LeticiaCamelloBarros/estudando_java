2dfdae5bcbedfd66652abd073e0d10bda52b09c3
leticia camello barros <lsncb@cesar.school>
2026-07-03 23:13:34 -0300

    Update Aluno.java
    havia colocado o código de pessoa em aluno

--- Escola_heranca/Aluno.java
+++ Escola_heranca/Aluno.java
@@ -1,34 +1,22 @@
 package escola;
 
-public class Pessoa {
-	//Pessoa é a superclasse das outras três 
-  private int idade ; 
-  @Override
-public String toString() {
-	return "Pessoa [idade=" + idade + ", sexo=" + sexo + ", nome=" + nome + "]";
-}
-  private char sexo ; 
-  private String nome ;
- //getters e setters ;  
-  public int getIdade() {
-	return idade;
-  }
-  public void setIdade(int idade) {
-	this.idade = idade;
-  }
-  public char getSexo() {
-	return sexo;
-  }
-  public void setSexo(char sexo) {
-	this.sexo = sexo;
-  }
-  public String getNome() {
-	return nome;
-  }
-  public void setNome(String nome) {
-	this.nome = nome;
-  } 
-  public void fazerAniv() {
-	  this.setIdade(this.getIdade()+1);
-  }
+public class Aluno extends Pessoa{
+    private int matricula;
+    private String curso ; 
+    public void cancelarMatricula() {
+    System.out.println("Matricula está sendo cancelada");
+    }
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+    
 }
