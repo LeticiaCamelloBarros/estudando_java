c337d50a44a3539f0e357801bfa9798aefd2d18d
leticia camello barros <lsncb@cesar.school>
2026-07-05 15:53:28 -0300

    transformando cachorro em um exemplo de sobrecarga

--- Fauna/Cachorro.java
+++ Fauna/Cachorro.java
@@ -2,17 +2,37 @@ package fauna;
 
 public class Cachorro extends Mamifero{
 
-	@Override
-	public void locomover() {
-		// TODO Auto-generated method stub
-      System.out.println("correr");
+public void reagir(String frase) {
+	if (frase.equals("toma comida") ||frase.equals("ola")) {
+		System.out.println("Abanar e latir");
+	}else {
+		System.out.println("rosnar");
 	}
-
-	@Override
-	public void emitirSom() {
-		// TODO Auto-generated method stub
-    System.out.println("au, au , au ");
+}
+public void reagir (boolean dono) {
+	if (dono) {
+		System.out.println("Latir e abanar");
+	}else {
+		System.out.println("rosnar e estranhar");
 	}
-	
+}
+public void reagir (int hora,int min) {
+         if (hora>=60) {
+        	 hora += (min/60);
+         }
+         if (hora>=5&&hora<=12) {
+        	 System.out.println("virar a barriga");
+         }else if (hora>=12&&hora<=18) {
+        	 System.out.println("abanar o rabo e correr");
+         }else if (hora>=12&&hora<=24) {
+        	 System.out.println("deitar e abanar rabo");
+         }else {
+        	 System.out.println("horario não existe !!");
+         }
+        	 
 
 }
+}
+
+
+
